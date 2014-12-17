8838a65874a593e0897ddde3717cdcba12217de8
guillaume <guillaumea@blinkbox.com>
2014-12-17 08:02:47 +0000

    Fix session_history to train item_stats on full train data set

--- recsys_challenge/predictors/session_history.py
+++ recsys_challenge/predictors/session_history.py
@@ -12,7 +12,7 @@ def predict_sessions(test_clicks, item_stats, n_clicks_threshold, rate_threshold
         on='ITEM_ID',
         right=item_stats[['BUYS/SESSIONS']].reset_index()
     )
-    predicted_mask = np.logical_or(
+    predicted_mask = np.logical_and(
         predicted_test_clicks.N_CLICKS >= n_clicks_threshold,
         predicted_test_clicks['BUYS/SESSIONS'] > rate_threshold)
         
@@ -39,6 +39,8 @@ if __name__ == '__main__':
         print evaluate(predicted_validation_sessions, validation_sessions)
     
     predicted_test_sessions = predict_sessions(test_clicks, item_stats, n_clicks_threshold=2, rate_threshold=0.05)
-        
-    with open('./solutions/session_history_2clicks_or_p5_solution.dat', 'w') as fp:
+
+    # 2clicks_and_p5 => ~25000
+    # 2clicks_or_p5 was better on the validation set but only ~15000 on the test
+    with open('./solutions/session_history_2clicks_and_p5_solution.dat', 'w') as fp:
         fp.writelines(session.to_csv_line() for session in predicted_test_sessions)
