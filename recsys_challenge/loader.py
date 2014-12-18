2607040499de3a4ff4024b2c92af24caf986692a
guillaume <guillaumea@blinkbox.com>
2014-12-18 08:46:02 +0000

    Introduced start/stop times in collapsed df

--- recsys_challenge/loader.py
+++ recsys_challenge/loader.py
@@ -1,32 +1,59 @@
-import pandas as pd
 import os
 
-def from_csv_in_directory(data_directory):
-    buys = pd.read_csv(os.path.join(data_directory, 'yoochoose-buys.dat'), names=['SESSION_ID', 'TS', 'ITEM_ID', 'PRICE', 'Q'])
-    clicks = pd.read_csv(os.path.join(data_directory, 'yoochoose-clicks.dat'), names=['SESSION_ID', 'TS', 'ITEM_ID', 'CATEGORY'])
-    test_clicks = pd.read_csv(os.path.join(data_directory, 'yoochoose-test.dat'), names=['SESSION_ID', 'TS', 'ITEM_ID', 'CATEGORY'])
-    return buys, clicks, test_clicks
+import pandas as pd
+import numpy as np
+
+BUYS_COLUMNS = ['SESSION_ID', 'TS', 'ITEM_ID', 'PRICE', 'Q']
+CLICKS_COLUMNS = ['SESSION_ID', 'TS', 'ITEM_ID', 'CATEGORY']
 
 
-def to_time_collapsed(buys, clicks, test_clicks):
+def time_collapsed_clicks(clicks):
+    """
+    >>> clicks_columns = ['SESSION_ID', 'TS', 'ITEM_ID', 'CATEGORY']
+    >>> clicks_logs = [[1, 't0', 10, 0], [1, 't1', 20, 0], [1, 't2', 20, 0]]
+    >>> clicks = pd.DataFrame(columns=clicks_columns, data=clicks_logs)
+
+    >>> time_collapsed_clicks(clicks)
+       SESSION_ID  ITEM_ID  CATEGORY ITEM_START ITEM_STOP  N_CLICKS SESSION_START SESSION_STOP
+    0           1       10         0         t0        t0         1            t0           t2
+    1           1       20         0         t1        t2         2            t0           t2
+    """
+    clicks_by_sessions = clicks.groupby(['SESSION_ID'])
+    session_limits = clicks_by_sessions.aggregate({'TS': [np.min, np.max]})
+    session_limits.columns = ['SESSION_START', 'SESSION_STOP']
+
+    clicks_by_item_and_sessions = clicks.groupby(['SESSION_ID', 'ITEM_ID'])
+    item_sub_sessions = clicks_by_item_and_sessions.aggregate({
+        'TS': [np.min, np.max],
+        'CATEGORY': np.min,
+        'SESSION_ID': len
+    })
+    # kind of fragile as based on column names alphabetical orders as dict keys?
+    item_sub_sessions.columns = ['CATEGORY', 'ITEM_START', 'ITEM_STOP', 'N_CLICKS']
+
+    collapsed_clicks = pd.merge(
+        left=item_sub_sessions.reset_index(),
+        right=session_limits.reset_index(),
+        on='SESSION_ID',
+        how='inner')
+    return collapsed_clicks
+
+    
+def collapse_time_and_join(buys, clicks):
     """ Collapsing the time dimension by grouping by (session_id, item_id)
     and then join clicks and buys rows.
     """
     buys = buys.groupby(['SESSION_ID', 'ITEM_ID']).size().reset_index()
     buys.columns = ['SESSION_ID', 'ITEM_ID', 'N_BUYS']
 
-    clicks = clicks.groupby(['SESSION_ID', 'ITEM_ID', 'CATEGORY']).size().reset_index()
-    clicks.columns = ['SESSION_ID', 'ITEM_ID', 'CATEGORY', 'N_CLICKS']
-
-    test_clicks = test_clicks.groupby(['SESSION_ID', 'ITEM_ID', 'CATEGORY']).size().reset_index()
-    test_clicks.columns = ['SESSION_ID', 'ITEM_ID', 'CATEGORY', 'N_CLICKS']
+    clicks = time_collapsed_clicks(clicks)
 
     clicks_and_buys = buys.merge(
         right=clicks, on=['SESSION_ID', 'ITEM_ID'],
         how='outer',
         suffixes=('_BUY', '_CLICKED')).sort('SESSION_ID').fillna(0)
 
-    return clicks_and_buys, test_clicks
+    return clicks_and_buys
 
 
 def to_pickled_df(data_directory, **kwargs):
@@ -36,6 +63,14 @@ def to_pickled_df(data_directory, **kwargs):
     
 if __name__ == '__main__':
     data_directory = 'data'
-    buys, clicks, test_clicks = from_csv_in_directory(data_directory)
-    clicks_and_buys, test_clicks = to_time_collapsed(buys, clicks, test_clicks)
-    to_pickled_df(data_directory, clicks_and_buys=clicks_and_buys, test_clicks=test_clicks)
+
+    buys = pd.read_csv(os.path.join(data_directory, 'yoochoose-buys.dat'), names=BUYS_COLUMNS)
+    clicks = pd.read_csv(os.path.join(data_directory, 'yoochoose-clicks.dat'), names=CLICKS_COLUMNS)
+    clicks_and_buys = collapse_time_and_join(buys, clicks)
+    to_pickled_df(data_directory, clicks_and_buys=clicks_and_buys)
+    
+    test_clicks = pd.read_csv(os.path.join(data_directory, 'yoochoose-test.dat'), names=CLICKS_COLUMNS)
+    test_clicks = time_collapsed_clicks(test_clicks)
+
+    to_pickled_df(data_directory, test_clicks=test_clicks)
+
